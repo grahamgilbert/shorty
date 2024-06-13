8a2ff02cb68835657e2187b97847f56be7c5abaa
Graham Gilbert <graham@grahamgilbert.com>
2024-06-13 10:54:34 +0100

    black

--- shorty.py
+++ shorty.py
@@ -37,17 +37,17 @@ def build_redirect_item(item, template_string, temp_directory):
     """
     Makes a directory and an index.html file with the redirect code in.
     """
-    if 'stub' not in item:
-        print('Item does not have a stub - exiting')
+    if "stub" not in item:
+        print("Item does not have a stub - exiting")
         sys.exit(1)
-    if 'url' not in item:
-        print('Item does not have a url - exiting')
+    if "url" not in item:
+        print("Item does not have a url - exiting")
         sys.exit(1)
 
-    stub_dir = os.path.join(temp_directory, item['stub'])
+    stub_dir = os.path.join(temp_directory, item["stub"])
     os.makedirs(stub_dir)
-    template_string = template_string.replace('THEURL', item['url'])
-    with open(os.path.join(stub_dir, 'index.html'), 'w') as html:
+    template_string = template_string.replace("THEURL", item["url"])
+    with open(os.path.join(stub_dir, "index.html"), "w") as html:
         html.write(template_string)
 
 
@@ -55,24 +55,24 @@ def generate_html(config, output, googleanalytics):
     """
     Builds the html
     """
-    template_dir = '/opt/shorty/templates'
+    template_dir = "/opt/shorty/templates"
     ensure_dir(output)
     if googleanalytics:
-        template = os.path.join(template_dir, 'with_google.html')
+        template = os.path.join(template_dir, "with_google.html")
     else:
-        template = os.path.join(template_dir, 'no_google.html')
+        template = os.path.join(template_dir, "no_google.html")
 
-    with open(template, 'r') as template_file:
+    with open(template, "r") as template_file:
         template_string = template_file.read()
 
     if googleanalytics:
-        template_string = template_string.replace('GOOGLEID', googleanalytics)
+        template_string = template_string.replace("GOOGLEID", googleanalytics)
 
     for item in config:
         build_redirect_item(item, template_string, output)
 
-    index_in = os.path.join(template_dir, 'index.html')
-    index_out = os.path.join(output, 'index.html')
+    index_in = os.path.join(template_dir, "index.html")
+    index_out = os.path.join(output, "index.html")
 
     shutil.copyfile(index_in, index_out)
 
@@ -81,16 +81,24 @@ def main():
 
     googleanalytics = None
 
-    parser = argparse.ArgumentParser(prog='Shorty',
-                                     description='This script will build a static '
-                                     'site with javascript redirections.')
+    parser = argparse.ArgumentParser(
+        prog="Shorty",
+        description="This script will build a static "
+        "site with javascript redirections.",
+    )
+    parser.add_argument("-c", "--config", help="Path to your config file. Required.")
     parser.add_argument(
-        '-c', '--config', help='Path to your config file. Required.')
-    parser.add_argument('-o', '--output', help='Path to output site to. Defaults to '
-                        'current working directory.', default=os.getcwd())
-    parser.add_argument('-g', '--google-analytics', help='ID for a Google Analytics account '
-                        'for tracking. Optional.',
-                        default=googleanalytics)
+        "-o",
+        "--output",
+        help="Path to output site to. Defaults to " "current working directory.",
+        default=os.getcwd(),
+    )
+    parser.add_argument(
+        "-g",
+        "--google-analytics",
+        help="ID for a Google Analytics account " "for tracking. Optional.",
+        default=googleanalytics,
+    )
     if len(sys.argv) < 2:
         parser.print_help()
         sys.exit(1)
@@ -103,5 +111,5 @@ def main():
     generate_html(parsed_config, output, googleanalytics)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
